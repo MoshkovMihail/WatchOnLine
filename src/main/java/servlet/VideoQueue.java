6fed0307f36c301e5869cf2972c83a434b0e700e
MoshkovMihail <mgmoshkov@kpfu.ru>
2025-10-10 23:50:24 +0300

    дз 4

--- src/main/java/servlet/VideoQueue.java
+++ src/main/java/servlet/VideoQueue.java
@@ -1,21 +1,59 @@
 package servlet;
 
+import models.Video;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
-@WebServlet("/room/queue")
+@WebServlet("/queue")
 public class VideoQueue extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("queue.html");
+        String htmlPath = getServletContext().getRealPath("/queue.html");
+        String htmlContent = new String(Files.readAllBytes(Paths.get(htmlPath)), StandardCharsets.UTF_8);
+
+        HttpSession session = req.getSession();
+        List<Video> listOfVideo = (List<Video>) session.getAttribute("listOfVideo");
+
+        PrintWriter pw = resp.getWriter();
+
+        pw.write(htmlContent);
+
+        for (Video video : listOfVideo) {
+            pw.write("<p>" + video.getLink() + "</p>");
+        }
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //
+        HttpSession session = req.getSession();
+
+        String videoRef = req.getParameter("video");
+        Video video = new Video(videoRef);
+
+        List<Video> listOfVideo = (List<Video>) session.getAttribute("listOfVideo");
+        if (listOfVideo == null) {
+            listOfVideo = new ArrayList<>();
+        }
+
+        listOfVideo.add(video);
+
+        req.setAttribute("listOfVideo", listOfVideo);
+        System.out.println("added");
+
     }
 }
